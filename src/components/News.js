9a47c940d634b313f52070f681cf1309a3707d81
Zishan Panchal <zpanchal2@gmail.com>
2024-10-10 19:39:13 -0600

    added a delay before requestes to adhere to API limits

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import NewsItem from "./NewsItem";
 import PropTypes from "prop-types";
 import Spinner from "./Spinner";
 import InfiniteScroll from "react-infinite-scroll-component";
+import throttle from 'lodash/throttle';
 
 export default function News(props) {
   const [articles, setArticles]= useState([]);
@@ -37,20 +38,43 @@ export default function News(props) {
     // eslint-disable-next-line
     updateNews()}, [])
  
-  const fetchMoreData = async () => {
-    const url = `https://api.newscatcherapi.com/v2/latest_headlines?not_sources=prnewswire.com&lang=en&when=24h&ranked_only&countries=${props.country}&topic=${props.category}&page_size=${props.pageSize}&page=${page+1}`;
-    setPage(page+1)
-    let data = await fetch(url,{
-      method:"GET",
-      headers:{
-        "x-api-key": props.apiKey,
-        "Access-Control-Allow-Origin":"*"
-      }
-    });
-    let parsedData = await data.json();
-    setArticles(articles.concat(parsedData.articles))
-    setTotalResults(parsedData.total_hits)
+  // const fetchMoreData = useCallback(throttle(async () => {
+  //   const url = `https://api.newscatcherapi.com/v2/latest_headlines?not_sources=prnewswire.com&lang=en&when=24h&ranked_only&countries=${props.country}&topic=${props.category}&page_size=${props.pageSize}&page=${page+1}`;
+  //   setPage(page+1)
+  //   let data = await fetch(url,{
+  //     method:"GET",
+  //     headers:{
+  //       "x-api-key": props.apiKey,
+  //       "Access-Control-Allow-Origin":"*"
+  //     }
+  //   });
+  //   let parsedData = await data.json();
+  //   setArticles(articles.concat(parsedData.articles))
+  //   setTotalResults(parsedData.total_hits)
+  //   }, 1500), [page, props.country, props.category, props.pageSize, props.apiKey]);
+  const fetchMoreData = useCallback(throttle(async () => {
+    const url = `https://api.newscatcherapi.com/v2/latest_headlines?not_sources=prnewswire.com&lang=en&when=24h&ranked_only&countries=${props.country}&topic=${props.category}&page_size=${props.pageSize}&page=${page + 1}`;
+
+    try {
+      let data = await fetch(url, {
+        method: "GET",
+        headers: {
+          "x-api-key": props.apiKey,
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+      let parsedData = await data.json();
+
+      setPage((prevPage) => prevPage + 1); // Safely update page using functional updates
+      setArticles((prevArticles) => prevArticles.concat(parsedData.articles)); // Concatenate new articles with old ones
+      setTotalResults(parsedData.total_hits);
+      // Add a delay to ensure no more than 1 request per second
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // Wait 3 second before allowing the next request
+    } catch (error) {
+      console.error("Error fetching more data:", error);
     }
+  }, 5000), [page, props.country, props.category, props.pageSize, props.apiKey]);
+    
     return (
       <>
         <h1 className="display-3 px-lg-5 mt-5 pt-4 my-3 position-relative w-75" style={{left:'7%'}}>Top {props.category.charAt(0).toUpperCase() + props.category.slice(1)} Headlines</h1>
